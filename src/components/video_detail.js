cf75b47b7e49342b7f3e5a3fe554d1ffba4b9bba
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-12-01 10:36:03 -0800

    Create ability to view a view from index.js.

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,13 +1,15 @@
 
 import React from 'react'
 
-const videoDetail = ({video}) => {  // let props = props.video  
+const VideoDetail = ({video}) => {  // let props = props.video  
+  if (!video) return <div>Loading...</div>
+
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
 
   return (
     <div className="video-detail col-md-8">
-      <div className="embed-responsive embed-responsive-16y9">
+      <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>    
       </div>
       <div className="details">
@@ -18,4 +20,4 @@ const videoDetail = ({video}) => {  // let props = props.video
   );
 };
 
-export default videoDetail
+export default VideoDetail
